39ed7e0706901eb6ef5aa7d56bb8ee3a6a424479
Josh Cordero <josh1703@gmail.com>
2025-10-10 08:48:13 -0600

    finalizado funcionalidad de login

--- src/app/layout/menu/menu.ts
+++ src/app/layout/menu/menu.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
@@ -18,11 +18,29 @@ import { RouterModule } from '@angular/router';
 })
 export class Menu {
 
+  loggin: boolean = true;
+
   @Output() toogleSidenav = new EventEmitter<void>();
+
+  constructor( private router: Router){
+
+  }
 
   /*Esto dispara el evento del sidenav*/
   onToggleSidenave() {
     this.toogleSidenav.emit();
+  }
+
+  isLogedIn(){
+    return this.loggin;
+  }
+
+  loginLogout(){
+    if(this.isLogedIn()){
+      this.router.navigate(['/login']);
+    }else{
+      
+    }
   }
 
 }
